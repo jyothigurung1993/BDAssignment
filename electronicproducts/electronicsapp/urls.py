03f14bedc2d25a766f3626f91bed855d67134e33
Jyothi Gurung <jyothigurung@gmail.com>
2022-02-01 19:26:51 +0530

    Add docstring comments

--- electronicproducts/electronicsapp/urls.py
+++ electronicproducts/electronicsapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path
 from .views import ElectronicProduct, ElectronicProducts
 
+"""
+Url for Performing CRUD on Electronic Product Details
+"""
 urlpatterns = [
     path('products/', ElectronicProducts.as_view()), #url for fetch product details
     re_path('product/(?P<product_id>[0-9a-z])/?$', ElectronicProduct.as_view())  # url for performing CRUD operation
--- electronicproducts/userapp/urls.py
+++ electronicproducts/userapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import MyObtainTokenPairView, RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
-
+"""
+Url for user login and registration
+"""
 urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
